ff670fd8253b5663a679bd592e2c4145128afe92
ptadiputra <ptadiputra12@gmail.com>
2023-05-16 08:41:44 +0800

    @ function contact store & + sysm link storage

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Auth;
 use Illuminate\Http\Request;
 use App\Models\Message;
 use App\Events\MessageSent;
+use Illuminate\Support\Facades\Log;
 
 class MessageController extends Controller
 {
@@ -13,13 +15,14 @@ class MessageController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index(){
-        $messages=Message::paginate(20);
-        return view('backend.message.index')->with('messages',$messages);
+    public function index()
+    {
+        $messages = Message::paginate(20);
+        return view('backend.message.index')->with('messages', $messages);
     }
     public function messageFive()
     {
-        $message=Message::whereNull('read_at')->limit(5)->get();
+        $message = Message::whereNull('read_at')->limit(5)->get();
         return response()->json($message);
     }
 
@@ -41,29 +44,40 @@ class MessageController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate($request,[
-            'name'=>'string|required|min:2',
-            'email'=>'email|required',
-            'message'=>'required|min:20|max:200',
-            'subject'=>'string|required',
-            'phone'=>'numeric|required'
+        $this->validate($request, [
+            'name' => 'string|required|min:2',
+            'email' => 'email|required',
+            'message' => 'required|min:20|max:200',
+            // 'subject' => 'string|required',
+            // 'phone' => 'numeric|required'
         ]);
-        // return $request->all();
+        $phone = '6287845289347'; // Ganti dengan nomor telepon tujuan
 
-        $message=Message::create($request->all());
-            // return $message;
-        $data=array();
-        $data['url']=route('message.show',$message->id);
-        $data['date']=$message->created_at->format('F d, Y h:i A');
-        $data['name']=$message->name;
-        $data['email']=$message->email;
-        $data['phone']=$message->phone;
-        $data['message']=$message->message;
-        $data['subject']=$message->subject;
-        $data['photo']=Auth()->user()->photo;
-        // return $data;    
-        event(new MessageSent($data));
-        exit();
+        // Format nomor telepon
+        // $phone = str_replace(['(', ')', ' ', '-'], '', $phone);
+        $message = 'Halo, admin. Saya ' . $request->name . '%0A' . 'email ' . $request->email . '%0A' . $request->message;
+
+
+        // URL WhatsApp dengan nomor tujuan
+        $url = "https://api.whatsapp.com/send?phone={$phone}&text={$message}";
+
+        // Pengalihan ke halaman WhatsApp
+        return redirect()->away($url);
+        // return $request->all();
+        // $message = Message::create($request->all());
+        // // return $message;
+        // $data = array();
+        // $data['url'] = route('message.show', $message->id);
+        // $data['date'] = $message->created_at->format('F d, Y h:i A');
+        // $data['name'] = $message->name;
+        // $data['email'] = $message->email;
+        // $data['phone'] = $message->phone;
+        // $data['message'] = $message->message;
+        // $data['subject'] = $message->subject;
+        // $data['photo'] = Auth()->user()->photo;
+        // // return $data;
+        // event(new MessageSent($data));
+        // exit();
     }
 
     /**
@@ -72,15 +86,14 @@ class MessageController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Request $request,$id)
+    public function show(Request $request, $id)
     {
-        $message=Message::find($id);
-        if($message){
-            $message->read_at=\Carbon\Carbon::now();
+        $message = Message::find($id);
+        if ($message) {
+            $message->read_at = \Carbon\Carbon::now();
             $message->save();
-            return view('backend.message.show')->with('message',$message);
-        }
-        else{
+            return view('backend.message.show')->with('message', $message);
+        } else {
             return back();
         }
     }
@@ -116,13 +129,12 @@ class MessageController extends Controller
      */
     public function destroy($id)
     {
-        $message=Message::find($id);
-        $status=$message->delete();
-        if($status){
-            request()->session()->flash('success','Successfully deleted message');
-        }
-        else{
-            request()->session()->flash('error','Error occurred please try again');
+        $message = Message::find($id);
+        $status = $message->delete();
+        if ($status) {
+            request()->session()->flash('success', 'Successfully deleted message');
+        } else {
+            request()->session()->flash('error', 'Error occurred please try again');
         }
         return back();
     }
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,126 +1,170 @@
 @extends('user_layouts.user_master')
 @section('content')
-<div class="container-fluid ">
-    <div class="row">
-        <div class="container">
-            <div class="row">
-                <div class="col-xs-12 col-lg-9">
-                    
-                    <!--
-                    MAIN INFO
-                    =============================================== -->
-                    <div class="row blog-item-page">
-                        
-                        <div class="col-md-12">
-                            
-                            
-        
-                             
-                            
-                            <!-- Info panel -->
-                            <div class="info-panel">
-                                <ul>
-                                    <li></li>
-                                    <li><a></a></li>
-                                    <li><a></a></li>
-                                </ul>
-                            </div>
-                             <!-- Header -->
-                             <div class="header">
-
-<section id="contact-us" class="contact-us section">
-    <div class="container">
-            <div class="contact-head">
+    <div class="container-fluid ">
+        <div class="row">
+            <div class="container">
                 <div class="row">
-                    <div class="col-lg-8 col-12">
-                        <div class="form-main">
-                            <div class="title">
-                                @php
-                                    
-                                @endphp
-                                <h4>Get in touch</h4>
-                                <h3>Write us a message @auth @else<span style="font-size:12px;" class="text-danger">[You need to login first]</span>@endauth</h3>
-                            </div>
-                            <form class="form-contact form contact_form" method="post" action="{{route('contact.store')}}" id="contactForm" novalidate="novalidate">
-                                @csrf
-                                <div class="row">
-                                    <div class="col-lg-6 col-12">
-                                        <div class="form-group">
-                                            <label>Your Name<span>*</span></label>
-                                            <input name="name" id="name" type="text" placeholder="Enter your name">
+                    <div class="col-xs-12 col-lg-9">
+
+                        <!--
+                                                                                                                    MAIN INFO
+                                                                                                                    =============================================== -->
+                        <div class="row blog-item-page">
+
+                            <div class="col-md-12">
+
+
+
+
+
+                                <!-- Info panel -->
+                                <div class="info-panel">
+                                    <ul>
+                                        <li></li>
+                                        <li><a></a></li>
+                                        <li><a></a></li>
+                                    </ul>
+                                </div>
+                                <!-- Header -->
+                                <div class="header">
+
+                                    <section id="contact-us" class="contact-us section">
+                                        <div class="container">
+                                            <div class="contact-head">
+                                                <div class="row">
+                                                    <div class="col-lg-8 col-12">
+                                                        <div class="form-main">
+                                                            <div class="title">
+                                                                @php
+                                                                    
+                                                                @endphp
+                                                                <h4>Get in touch</h4>
+                                                            <h3>Write us a message @auth @else<span
+                                                                        style="font-size:12px;" class="text-danger">[You
+                                                                    need to login first]</span>@endauth
+                                                            </h3>
+                                                        </div>
+                                                        <form class="form-contact form contact_form" method="post"
+                                                            action="{{ route('contact.store') }}" id="contactForm"
+                                                            novalidate="novalidate">
+                                                            @csrf
+                                                            <div class="row">
+                                                                <div class="col-lg-6 col-12">
+                                                                    <div class="form-group">
+                                                                        <label>Your Name<span>*</span></label>
+                                                                        <input name="name" id="name"
+                                                                            type="text" placeholder="Enter your name"
+                                                                            class="@error('name') is-invalid @enderror"
+                                                                            value="{{ old('name') }}">
+                                                                    </div>
+                                                                    @error('name')
+                                                                        <div class="alert alert-danger">{{ $message }}
+                                                                        </div>
+                                                                    @enderror
+                                                                </div>
+                                                                {{-- <div class="col-lg-6 col-12">
+                                                                    <div class="form-group">
+                                                                        <label>Your Subjects<span>*</span></label>
+                                                                        <input name="subject" type="text"
+                                                                            id="subject" placeholder="Enter Subject"
+                                                                            class="@error('subject') is-invalid @enderror">
+                                                                    </div>
+                                                                    @error('subject')
+                                                                        <div class="alert alert-danger">{{ $message }}
+                                                                        </div>
+                                                                    @enderror
+                                                                </div> --}}
+                                                                <div class="col-lg-6 col-12">
+                                                                    <div class="form-group">
+                                                                        <label>Your Email<span>*</span></label>
+                                                                        <input name="email" type="email"
+                                                                            id="email"
+                                                                            placeholder="Enter email address"
+                                                                            class="@error('email') is-invalid @enderror"
+                                                                            value="{{ old('email') }}">
+                                                                    </div>
+                                                                    @error('email')
+                                                                        <div class="alert alert-danger">{{ $message }}
+                                                                        </div>
+                                                                    @enderror
+                                                                </div>
+                                                                {{-- <div class="col-lg-6 col-12">
+                                                                    <div class="form-group">
+                                                                        <label>Your Phone<span>*</span></label>
+                                                                        <input id="phone" name="phone"
+                                                                            type="number"
+                                                                            placeholder="Enter your phone"
+                                                                            class="@error('phone') is-invalid @enderror">
+                                                                    </div>
+                                                                    @error('phone')
+                                                                        <div class="alert alert-danger">{{ $message }}
+                                                                        </div>
+                                                                    @enderror
+                                                                </div> --}}
+                                                                <div class="col-12">
+                                                                    <div class="form-group message">
+                                                                        <label>your message<span>*</span></label>
+                                                                        <textarea name="message" id="message" cols="30" rows="9" placeholder="Enter Message"
+                                                                            class="@error('message') is-invalid @enderror">{{ old('message') }}</textarea>
+                                                                    </div>
+                                                                    @error('message')
+                                                                        <div class="alert alert-danger">{{ $message }}
+                                                                        </div>
+                                                                    @enderror
+                                                                </div>
+                                                                <div class="col-12">
+                                                                    <div class="form-group button">
+                                                                        <button type="submit" class="btn-primary"
+                                                                            target="_blank">Send
+                                                                            Message</button>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+
+                                            </div>
                                         </div>
                                     </div>
-                                    <div class="col-lg-6 col-12">
-                                        <div class="form-group">
-                                            <label>Your Subjects<span>*</span></label>
-                                            <input name="subject" type="text" id="subject" placeholder="Enter Subject">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6 col-12">
-                                        <div class="form-group">
-                                            <label>Your Email<span>*</span></label>
-                                            <input name="email" type="email" id="email" placeholder="Enter email address">
-                                        </div>	
-                                    </div>
-                                    <div class="col-lg-6 col-12">
-                                        <div class="form-group">
-                                            <label>Your Phone<span>*</span></label>
-                                            <input id="phone" name="phone" type="number" placeholder="Enter your phone">
-                                        </div>	
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="form-group message">
-                                            <label>your message<span>*</span></label>
-                                            <textarea name="message" id="message" cols="30" rows="9" placeholder="Enter Message"></textarea>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="form-group button">
-                                            <button type="submit" class="btn-primary">Send Message</button>
+                                </section>
+                                <!--/ End Contact -->
+
+                                <!--================Contact Success  =================-->
+                                <div class="modal fade" id="success" tabindex="-1" role="dialog"
+                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h2 class="text-success">Thank you!</h2>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p class="text-success">Your message is successfully sent...</p>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </form>
-                        </div>
-                    </div>
-                    
-                </div>
-            </div>
-        </div>
-</section>
-<!--/ End Contact -->
 
-<!--================Contact Success  =================-->
-<div class="modal fade" id="success" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-            <h2 class="text-success">Thank you!</h2>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <p class="text-success">Your message is successfully sent...</p>
-        </div>
-      </div>
-    </div>
-</div>
-
-<!-- Modals error -->
-<div class="modal fade" id="error" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-            <h2 class="text-warning">Sorry!</h2>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
-        <div class="modal-body">
-            <p class="text-warning">Something went wrong.</p>
-        </div>
-      </div>
-    </div>
-</div>
-@endsection
+                                <!-- Modals error -->
+                                <div class="modal fade" id="error" tabindex="-1" role="dialog"
+                                    aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h2 class="text-warning">Sorry!</h2>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p class="text-warning">Something went wrong.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endsection
